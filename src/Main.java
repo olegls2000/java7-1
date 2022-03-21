003302b924cb1a722c6b5c7352ff0a5d6814a78d
Julia Kull <7102777@mail.ru>
2022-03-21 21:16:23 +0200

    my first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello W");
+        System.out.println("Hello Wo");
+        System.out.println("Hello Julia");
     }
 }
