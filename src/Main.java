bad1b00b6e20d3233f594d878eecd4df12dcf9ac
yenik <nikandrov15@gmail.com>
2022-03-21 21:19:33 +0200

    Update to - forloop

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello W");
         System.out.println("Hello Jegor");
+        int a = 20;
+        int i = 0;
+        for(i=0;i<a; i++){
+            System.out.println("hello");
+
+        }
+
+
     }
 }
