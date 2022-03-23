d568cb69c31dc8e87e025a4cb5d390bb0017d92c
Marina B <brik.marina@gmail.com>
2022-03-23 19:28:29 +0200

    Lesson4

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr=getArrayTill30(30);
+        int[] arr = getArrayTill30(30);
         printArray(arr);
         printInverseArray(arr);
+        System.out.println("Sum = " + getSumOfElements(arr));
     }
+
+    public static int getSumOfElements(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; ++i) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
     public static int[] getArrayTill30(int count) {
-        int[] arr=new int[15];
-        arr=oddNumbers(arr,count);
+        int[] arr = new int[15];
+        arr = oddNumbers(arr, count);
 
         return arr;
     }
-    public static int[] oddNumbers(int[] arr,int count) {
+
+    public static int[] oddNumbers(int[] arr, int count) {
 
         for (int i = 0, k = 0; i <= count; ++i) {
-            if (i % 2 ==1) {
+            if (i % 2 == 1) {
                 arr[k] = i;
                 k++;
             }
         }
         return arr;
     }
-
 
 
     public static void printInverseArray(int[] arr) {
