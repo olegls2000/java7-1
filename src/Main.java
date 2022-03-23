3a268ce8b3cb1f472085438cb86ef3c773a60815
oleg.pasichnyk <oleg.pasichnyk@kuehne-nagel.com>
2022-03-23 19:36:30 +0200

    Lesson4 formatting

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
 //printOutArray(getArrayTill30());
 
-        int [] array = getArrayTill30();
+        int[] array = getArrayTill30();
         int sum = getSumOfElements(array);
         System.out.println(sum);
     }
 
-    public static int [] getArrayTill30() {
+    public static int[] getArrayTill30() {
         int[] result = new int[15];
         int count = 0;
         int i = 0;
@@ -18,19 +18,19 @@ public class Main {
             }
             count++;
         }
-        for (int j = result.length-1; j>=0; j--) {
-            System.out.print(result[j] +" ");
+        for (int j = result.length - 1; j >= 0; j--) {
+            System.out.print(result[j] + " ");
         }
         System.out.println();
         return result;
     }
 
 
-    public static int getSumOfElements(int [] array){
-    int sum = 0;
+    public static int getSumOfElements(int[] array) {
+        int sum = 0;
         for (int i = 0; i < array.length; i++) {
-            sum = sum + array[i];
+            sum += array[i];
         }
-return sum;
+        return sum;
     }
 }
