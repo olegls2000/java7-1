6d357b2ec953b4236daf26b9b275f727fbecf8b6
Julia Kull <7102777@mail.ru>
2022-03-23 20:18:02 +0200

    Lesson 4 new method

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ public class Main {
         int[] array = getArrayTill30();
         int sum = getSumOfElements(array);
         System.out.println(sum);
+        int [] array1 = {3,5,9,8,66};
+        System.out.println(findMaxElement(array1));
+        System.out.println(findMinElement(array1));
+
     }
 
     public static int[] getArrayTill30() {
@@ -32,5 +36,22 @@ public class Main {
             sum += array[i];
         }
         return sum;
+    }
+    public static int findMaxElement(int[] array){
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]>max)
+            max = array[i];
+        }
+        return max;
+    }
+
+    public static int findMinElement(int[] array){
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]<min)
+                min = array[i];
+        }
+        return min;
     }
 }
