208f8b0273e42de5611ff4d957e47e50d3659784
enekoe <enekose@gmail.com>
2022-03-25 00:13:50 +0200

    Lesson4HomeWorkTask2

--- src/HomeWorkLesson4.java
+++ src/HomeWorkLesson4.java
@@ -1,7 +1,9 @@
 public class HomeWorkLesson4 {
     public static void main(String[] args) {
         double[] arrayAverage = {7, 9, 17, 4, 50};
+        double[][] array2D = {{1, 5, 98, 12}, {55, 3, 45, 77, 2, 17}};
         getAverage(arrayAverage);
+        average2D(array2D);
     }
 
     public static double getAverage(double[] array) {
@@ -10,7 +12,19 @@ public class HomeWorkLesson4 {
             summ += array[i];
         }
         double average = summ / array.length;
-        System.out.println(average);
+        System.out.println("Average of array is: " + average);
         return average;
+    }
+
+    public static double average2D(double[][] array) {
+        double summ = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                summ += array[i][j];
+            }
+        }
+        double result = summ / (array[0].length + array[1].length);
+        System.out.println("Average of 2D array is: " + result);
+        return summ;
     }
 }
