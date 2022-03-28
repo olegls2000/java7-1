c282cd152e2f8f0dd137ec74d7679aa4e9e2142f
yenik <nikandrov15@gmail.com>
2022-03-28 12:39:37 +0300

    Lesson4 массивы/ поиск макс. и мин. элементов

--- src/Lesson4.java
+++ src/Lesson4.java
@@ -3,6 +3,56 @@ public class Lesson4 {
         int[] arrayTill30 = getArrayTill30();
         int summ = getSummOfElems(arrayTill30);
         System.out.println("sum =" + summ);
+
+
+        int[] array = {6, 9, 8, 15, 99, 22};
+        int max = findMaxElem(array);
+        int min = findMinElems(array);
+
+
+        int[][] matrix = {
+                {10, 11, 12},
+                {20, 21, 22},
+                {30, 31, 32}
+        };
+        //System.out.println("[");
+        for (int i = 0; i < matrix.length; i++) {
+            //System.out.print("[");
+            for (int j = 0; j < matrix.length; j++) {
+                //System.out.print(matrix[i][j] + ", ");
+
+            }
+            //System.out.println("]");
+
+        }
+        //System.out.println("]");
+        int[] arrayForSwap = {244, 55, 16, 55};
+        int[] arraySwapped = swapMaxAndMin(arrayForSwap);
+    }
+
+    public static int[] swapMaxAndMin(int[] array) {
+        int iMax = 0;
+        for (int i = 1; i < array.length; i++) {
+
+            if (array[i] > array[iMax]) {
+                iMax = i;
+            }
+        }
+        System.out.println("Max element index: " + iMax + ", Value = " + array[iMax]);
+        int iMin = 0;
+        for (int i = 1; i < array.length; i++) {
+
+            if (array[i] < array[iMin]) {
+                iMin = i;
+
+            }
+        }
+        System.out.println("Min element index: " + iMin + ", Value = " + array[iMin]);
+        int temp = array[iMax];
+        array[iMax] = array[iMin];//1,2,3,1,4
+        array[iMin] = temp;// 9,2,3,1,4
+
+        return array;
     }
 
     public static int[] getArrayTill30() {
@@ -31,4 +81,27 @@ public class Lesson4 {
         return sum;
     }
 
+    public static int findMaxElem(int[] array) {
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+
+        }
+        System.out.println("Max element is: " + max);
+        return max;
+    }
+
+    public static int findMinElems(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+
+        }
+        System.out.println("Minimum is: " + min);
+        return min;
+    }
 }
