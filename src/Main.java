f3a588d395d013e48f061193e87ba92900b3b79f
Marina B <brik.marina@gmail.com>
2022-03-28 15:55:13 +0300

    Lesson4 HW

--- src/Main.java
+++ src/Main.java
@@ -9,39 +9,52 @@ public class Main {
         System.out.println("Min = " + findMinElement(arr1));
 
         int[][] matrix = {
-                {10, 9, 7, 1},
-                {2, 7, 9, 8},
-                {6, 8, 7, 7}
+                {1, 4, 7, 9},
+                {2, 7, 11, 8},
+                {6, 10, 0, 3}
         };
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
 
-            }
-            System.out.println();
-        }
         int[] arrayForSwap = {1, 2, 3, 4, 7, 9};
-        int[] arr2=swapMinAndMax(arrayForSwap);
-        printArray(arr2);
+        swapMinAndMax(arrayForSwap);
+        printArray(arrayForSwap);
+
+        System.out.println("Avarage in 1D: " + findAverage1D(arr1));
+        System.out.println("Avarage in 2D: " + findAverage2D(matrix));
+        System.out.println("Minimum in 2D: " + findMin2D(matrix));
+        System.out.println("Maximum in 2D: " + findMax2D(matrix));
+        printMatrix(matrix, "Before MinAndMax swapping:");
+        swapMinAndMaxInMatrix(matrix);
+        printMatrix(matrix, "After MinAndMax swapping:");
+
+        putMinFirst2D(matrix);
+        printMatrix(matrix, "After MinElFirst modification:");
+
+        int[][] matrix1 = {
+                {5, 4, 3, 9},
+                {2, 1, 11, 8},
+                {6, 10, 4, 3}
+        };
+        printMatrix(matrix1, "Before MinElFirst(s) modification:");
+        putMinFirst2Dsimple(matrix1);
+        printMatrix(matrix1, "After MinElFirst(s) modification:");
     }
 
-    public static int[] swapMinAndMax(int[] arr) {
+    public static void swapMinAndMax(int[] arr) {
         int imin = 0;
         int imax = 0;
         for (int i = 1; i < arr.length; ++i) {
-                if (arr[i] < arr[imin]) {
-                    imin = i;
+            if (arr[i] < arr[imin]) {
+                imin = i;
             }
             if (arr[i] > arr[imax]) {
                 imax = i;
             }
         }
-        System.out.println("Max: array["+imax+"] = "+arr[imax]);
-        System.out.println("Min: array["+imin+"] = "+arr[imin]);
-        int tmp=arr[imin];
-        arr[imin]=arr[imax];
-        arr[imax]=tmp;
-        return arr;
+        System.out.println("Max: array[" + imax + "] = " + arr[imax]);
+        System.out.println("Min: array[" + imin + "] = " + arr[imin]);
+        int tmp = arr[imin];
+        arr[imin] = arr[imax];
+        arr[imax] = tmp;
     }
 
     public static int findMinElement(int[] arr) {
@@ -51,7 +64,6 @@ public class Main {
                 min = arr[i];
             }
         }
-
         return min;
     }
 
@@ -76,12 +88,11 @@ public class Main {
 
     public static int[] getArrayTill30(int count) {
         int[] arr = new int[15];
-        arr = oddNumbers(arr, count);
-
+        oddNumbers(arr, count);
         return arr;
     }
 
-    public static int[] oddNumbers(int[] arr, int count) {
+    public static void oddNumbers(int[] arr, int count) {
 
         for (int i = 0, k = 0; i <= count; ++i) {
             if (i % 2 == 1) {
@@ -89,7 +100,6 @@ public class Main {
                 k++;
             }
         }
-        return arr;
     }
 
 
@@ -132,8 +142,153 @@ public class Main {
             } else {
                 s = true;
             }
-
         }
         System.out.println(" ]");
     }
+
+    public static int[] findMinAndMaxInMatrix(int[][] matrix) {
+        int imax = 0, jmax = 0;
+        int imin = 0, jmin = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (!(i == 0 && j == 0)) {
+                    if (matrix[i][j] < matrix[imin][jmin]) {
+                        imin = i;
+                        jmin = j;
+                    }
+                    if (matrix[i][j] > matrix[imax][jmax]) {
+                        imax = i;
+                        jmax = j;
+                    }
+                }
+            }
+
+        }
+        return new int[]{imin, jmin, imax, jmax};
+    }
+
+    public static void swapMinAndMaxInMatrix(int[][] matrix) {
+        int[] minmaxIndexes = findMinAndMaxInMatrix(matrix);
+
+        int imin = minmaxIndexes[0];
+        int jmin = minmaxIndexes[1];
+        int imax = minmaxIndexes[2];
+        int jmax = minmaxIndexes[3];
+
+        int temp = matrix[imin][jmin];
+        matrix[imin][jmin] = matrix[imax][jmax];
+        matrix[imax][jmax] = temp;
+    }
+
+    public static void printMatrix(int[][] matrix, String message) {
+        System.out.println(message);
+        System.out.print("[ ");
+        for (int i = 0; i < matrix.length; i++) {
+            boolean s = false;
+            if (i > 0)
+                System.out.print("  ");
+            System.out.print("[ ");
+
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (s) {
+                    System.out.print(", ");
+                } else {
+                    s = true;
+                }
+                System.out.print(matrix[i][j]);
+            }
+            System.out.print(" ]");
+            if (i != matrix.length - 1) {
+                System.out.println();
+            }
+        }
+        System.out.println(" ]");
+    }
+
+    public static double findAverage1D(int[] array) {
+        double avg = 0.0;
+        for (int i = 0; i < array.length; i++) {
+            avg += array[i];
+        }
+        return avg / array.length;
+    }
+
+    public static double findAverage2D(int[][] matrix) {
+        double avg = 0.0;
+        int elCount = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                avg += matrix[i][j];
+            }
+            elCount += matrix[i].length;
+        }
+        return avg / elCount;
+    }
+
+    public static int findMin2D(int[][] matrix) {
+        int min = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i == 0 && j == 0) {
+                    min = matrix[0][0];
+                } else if (matrix[i][j] < min) {
+                    min = matrix[i][j];
+                }
+            }
+        }
+        return min;
+    }
+
+    public static int findMax2D(int[][] matrix) {
+        int max = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i == 0 && j == 0) {
+                    max = matrix[0][0];
+                } else if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void putMinFirst2Dsimple(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            int jmin = 0;
+            for (int j = 0; j < matrix[i].length; ++j) {
+                if (j != 0) {
+                    if (matrix[i][j] < matrix[i][jmin]) {
+                        jmin = j;
+                    }
+                }
+            }
+            int temp = matrix[i][0];
+            matrix[i][0] = matrix[i][jmin];
+            matrix[i][jmin] = temp;
+        }
+    }
+
+    public static int findMinIndex(int[] array) {
+        int imin = 0;
+        for (int i = 0; i < array.length; ++i) {
+            if (i != 0) {
+                if (array[i] < array[imin]) {
+                    imin = i;
+                }
+            }
+        }
+        return imin;
+    }
+
+    public static void putMinFirst2D(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            int imin = findMinIndex(matrix[i]);
+            int temp = matrix[i][0];
+            matrix[i][0] = matrix[i][imin];
+            matrix[i][imin] = temp;
+        }
+    }
 }
+
+
