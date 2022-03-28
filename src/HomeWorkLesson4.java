6ff16f7c29d5a4c030e83ab04fd8ae94dd5adfe4
enekoe <enekose@gmail.com>
2022-03-28 13:36:55 +0300

    Lesson4HomeWorkTask6

--- src/HomeWorkLesson4.java
+++ src/HomeWorkLesson4.java
@@ -1,13 +1,16 @@
 public class HomeWorkLesson4 {
     public static void main(String[] args) {
         int[] arrayAverage = {7, 9, 17, 4, 50};
-        int[][] array2D = {{1, 50, 9800, 12}, {55000, 300, 45, 70, 2000, 7}};
+        int[][] array2D = {{10, 50, 98000, 12}, {55000, 300, 45, 70, 2000, 7}};
+        int[][] array2Dref = {{50,10,56,60}, {10,45,5,99}};
+        int[][] reformat2D = reformatArray(array2Dref);
         int[][] arraySwapped = swapMaxAndMin(array2D);
         getAverage(arrayAverage);
         average2D(array2D);
         minOf2DArray(array2D);
         maxOf2DArray(array2D);
         printOutArray(arraySwapped);
+        printOutArray(reformat2D);
     }
 
     public static double getAverage(int[] array) {
@@ -85,6 +88,24 @@ public class HomeWorkLesson4 {
         return array;
     }
 
+    public static int[][] reformatArray(int[][] array) {
+        int iMin = 0;
+        int jMin = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] < array[iMin][jMin]) {
+                    iMin = i;
+                    jMin = j;
+                }
+            }
+        }
+        int tmp = array[iMin][jMin];
+        array[iMin][jMin] = array[0][0];
+        array[0][0] = tmp;
+        ;
+        return array;
+    }
+
     public static void printOutArray(int[][] array) {
 
         for (int i = 0; i < array.length; i++) {
@@ -94,5 +115,6 @@ public class HomeWorkLesson4 {
             }
             System.out.print("] ");
         }
+        System.out.println("");
     }
 }
